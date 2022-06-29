adf02cef1f26743dec115fe9ae031d4b920d15a8
Des Hartman <des@deshartman.com>
2022-06-29 17:16:21 +1000

    updated uui and CallSid exttraction

--- dcs-calls/functions/outboundHandler.js
+++ dcs-calls/functions/outboundHandler.js
@@ -1,32 +1,32 @@
 /**
  * This is the outbound to PSTN voice handler that routes the call from the Customer to a PSTN destination.
- * The PSTN side call SID is written into a Sync map as reference, so Pay can be attached.
+ * The PSTN side call SID is provided as part of the call back as reference, so Pay can be attached.
  *
- * Once the call is connected, we call the StatusCallback to write the callSID in the UUI sync map.
+ * Once the call is connected, we call the StatusCallback
  * 
  */
 exports.handler = function (context, event, callback) {
 
     const voiceResponse = new Twilio.twiml.VoiceResponse();
-    const restClient = context.getTwilioClient();
 
-    // Extract the UUI in the parent call leg (PBX to Twilio). The PSTN side call SID will be in the callback body. 
-    // See https://www.twilio.com/docs/voice/api/call-resource#parameters-sent-to-your-statuscallback-url
+    let to = event.To.match(/^sip:((\+)?[0-9]+)@(.*)/)[1];
+    let from = event.From.match(/^sip:((\+)?[0-9]+)@(.*)/)[1];
+
+    // console.log(`outboundHandler Event Details: ${JSON.stringify(event, null, 4)}`);
+
+    // Extract the UUI in the parent call leg (PBX to Twilio). The PSTN side call SID will be in the callback body.
+    let uui = event["SipHeader_User-to-User"];
+    if (!uui) {
+        return callback(null, 'Cannot extract UUI from call, so cannot establish Pay for callSID: ' + event.CallSid);
+    };
+
+
     try {
-        var parentCall = await restClient.calls(event.ParentCallSid).fetch();
-
-        const paramPart = parentCall.to.split("?")[1];
-        const params = new URLSearchParams(paramPart);
-        const uui = params.get('User-to-User');
-
-        if (!uui) {
-            return callback(null, 'Cannot extract UUI from call, so cannot establish Pay for callSID: ' + event.CallSid);
-        }
         // console.log(`Dialing ${to} with Caller ID ${from} - Was to:${event.To} from:${event.From}`);
         const dial = voiceResponse.dial({ callerId: from });
         dial.number(
             {
-                // Only update Sync when call is answered
+                // Only update when call is answered
                 statusCallbackEvent: 'answered',
                 statusCallback: context.CALLBACK_URL + '?uui=' + uui,
                 statusCallbackMethod: 'POST'
@@ -34,7 +34,6 @@ exports.handler = function (context, event, callback) {
             to);
 
         return callback(null, voiceResponse);
-
     } catch (error) {
         return callback(null, `Error with OutboundHandler: ${error}`);
     }
--- dcs-calls/functions/outboundLog.js
+++ dcs-calls/functions/outboundLog.js
@@ -1,11 +1,15 @@
 /**
- * This is a mock function, to just log the UUI received from Genesys.
+ * This is a mock function, to just log the UUI received from Genesys and the Twilio call details, specifically
+ * the PSTN side callSID.
  * 
  */
 exports.handler = async function (context, event, callback) {
 
     // log the call
-    console.log("Received the following data: " + JSON.stringify(event));
+    console.log("Received the following data: " + JSON.stringify(event, null, 4));
+
+    console.log(`outboundLog PSTN Call SID: ${event.CallSid}`);
+    console.log(`outboundLog UUI: ${event.uui}`);
 
 
     return callback(null, event);
